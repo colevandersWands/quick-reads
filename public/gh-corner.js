8ef0ef105ce69ec46f3ece3205b2e524cc9f7310
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2025-12-05 06:14:33 -0600

    fix source link logic, add title to README

--- public/gh-corner.js
+++ public/gh-corner.js
@@ -3,6 +3,9 @@ const GITHUB_REPO = "https://github.com/colevandersWands";
 document
   .getElementById("gh-corner")
   .addEventListener("click", function openPageSource() {
-    const quickReadSourceURL = GITHUB_REPO + window.location.pathname;
+    const quickReadSourceURL = `${GITHUB_REPO}/${window.location.pathname
+      .split("/")
+      .filter(Boolean)
+      .join("/tree/main/")}`;
     window.open(quickReadSourceURL, "_blank");
   });
